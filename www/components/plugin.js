ce461feb8a98db6d882249b885e9383eaee3b754
henrique24201 <hmoura050705@gmail.com>
2024-10-26 09:03:05 +0900

    alteraçoes nos botoes

--- www/components/plugin.js
+++ www/components/plugin.js
@@ -1,2 +1,27 @@
 // This is a JavaScript file
 
+$(document).on("click","#alerta", function(){
+ 
+     function alertDismissed(){
+    }
+    navigator.notification.alert("aleatório", alertDismissed,"alerta");
+});
+ 
+$(document).on("click","#confirm", function(){
+    function confirma(buttonIndex){
+        if(buttonIndex == "1"){    
+        navigator.notification.alert("Escolheu a opção A");
+        }else{
+        navigator.notification.alert("Escolheu a opção B");
+        }
+    }
+    navigator.notification.confirm("Escolha A ou B", confirma,"Escolha",['A','B']);
+});
+ 
+$(document).on("click","#beep", function(){
+    navigator.notification.beep(4);
+});
+ 
+$(document).on("click","#vibra", function(){
+    navigator.vibrate([1000, 1000, 3000, 1000, 5000]);
+});
